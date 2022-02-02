dbc1bc54bb38b9727c94cb6916f701fbfaf5b6ca
liobrdev <contact@liobr.dev>
2022-02-01 23:20:29 -0500

    Updates AccountFormDeleteUser.tsx.

--- frontend/components/AccountFormDeleteUser.tsx
+++ frontend/components/AccountFormDeleteUser.tsx
@@ -1,50 +1,36 @@
-import { FormEvent, MouseEvent, useRef, useState } from 'react';
+import { Component, FormEvent, MouseEvent, createRef, RefObject } from 'react';
+import { connect, ConnectedProps } from 'react-redux';
+
 import { mutate } from 'swr';
 import { enableBodyScroll, disableBodyScroll } from 'body-scroll-lock';
 
-import { useAppDispatch, useAppSelector } from '@/hooks';
+import { AppDispatch, AppState } from '@/types';
 import { parseErrorResponse, request } from '@/utils';
 
 import { Input, CloseIcon } from './';
 
 
-const initialForm = { email: '', current_password: '' };
-const initialError: ReturnType<typeof parseErrorResponse> = {};
+class AccountFormDeleteUser extends Component<Props, State> {
+  private component: RefObject<HTMLDivElement>;
 
-export default function AccountFormDeleteUser() {
-  const { formOnDeleteUser } = useAppSelector((state) => state.account);
-  const {
-    isLoggingIn,
-    isLoggingOut,
-    isRegistering,
-    isUpdating,
-    isDeleting,
-    user,
-  } = useAppSelector((state) => state.user);
-  const dispatch = useAppDispatch();
-
-  const [form, setForm] = useState({ ...initialForm });
-  const [error, setError] = useState({ ...initialError });
-
-  const thisComponent = useRef<HTMLDivElement>(null);
-
-  const handleShow = (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    setForm({ ...initialForm });
-    setError({});
-    if (thisComponent.current) disableBodyScroll(thisComponent.current);
-    dispatch({ type: 'ACCOUNT_FORM_DELETE_SHOW' });
+  constructor(props: Props) {
+    super(props);
+    this.component = createRef();
+    this.state = {
+      form: { email: '', current_password: '' },
+      error: {},
+    };
+    this.handleClose = this.handleClose.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  const handleClose = (e: MouseEvent<HTMLButtonElement | HTMLDivElement>) => {
+  handleClose(e: MouseEvent<HTMLButtonElement | HTMLDivElement>) {
     e.preventDefault();
-    setForm({ ...initialForm });
-    setError({});
-    if (thisComponent.current) enableBodyScroll(thisComponent.current);
-    dispatch({ type: 'ACCOUNT_FORM_DELETE_CLOSE' });
+    this.props.accountFormDeleteClose();
   }
 
-  const handleInput = () => (e: FormEvent<HTMLInputElement>) => {
+  handleInput(e: FormEvent<HTMLInputElement>) {
     e.preventDefault();
     let name = (e.target as HTMLInputElement).name;
     
@@ -53,148 +39,173 @@ export default function AccountFormDeleteUser() {
     else return;
 
     const value = (e.target as HTMLInputElement).value;
-    setForm((prevState) => ({ ...prevState, [name]: value }));
+    this.setState({ form: { ...this.state.form, [name]: value } });
   };
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+  async handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    dispatch({ type: 'START_DELETE_USER' });
-    setError({});
+    this.props.startDeleteUser();
+    this.setState({ error: {} });
+    const { form } = this.state;
 
     try {
-      const token = localStorage.getItem('simple_kanban_token');
+      const token = localStorage.getItem('simplepasswords_token');
       await request
-        .delete(`/users/${user?.user_slug}/`)
+        .delete(`/users/${this.props.user?.user_slug}/`)
         .set({ 'Authorization': `Token ${token}` })
         .send({ ...form });
-      setForm({ ...initialForm });
-      localStorage.removeItem('simple_kanban_token');
-      if (thisComponent.current) enableBodyScroll(thisComponent.current);
-      dispatch({ type: 'SUCCESS_DELETE_USER' });
+      this.setState({ form: { email: '', current_password: '' } });
+      localStorage.removeItem('simplepasswords_token');
+      if (this.component.current) enableBodyScroll(this.component.current);
+      this.props.successDeleteUser();
       mutate('/users/');
     } catch (err: any) {
-      dispatch({ type: 'STOP_DELETE_USER' });
+      this.props.stopDeleteUser();
       // Possibly report to api in the future, for now just console.error
       // reportErrorToAPI(err);
       console.error(err);
+      err = err?.response;
 
-      if (
-        err?.response?.unauthorized ||
-        err?.response?.forbidden ||
-        err?.response?.notFound
-      ) {
-        localStorage.removeItem('simple_kanban_token');
-        if (thisComponent.current) enableBodyScroll(thisComponent.current);
-        dispatch({ type: 'START_LOGOUT_USER' });
+      if (err?.unauthorized || err?.forbidden || err?.notFound) {
+        localStorage.removeItem('simplepasswords_token');
+        if (this.component.current) enableBodyScroll(this.component.current);
+        this.props.startLogoutUser();
       }
-
-      setError(parseErrorResponse(err?.response?.body, Object.keys(form)));
+      const error = parseErrorResponse(err?.body, Object.keys(form));
+      this.setState({ error });
     } finally {
       (document.activeElement as HTMLElement).blur();
     }
   };
 
-  const canSubmit = !!(
-    formOnDeleteUser &&
-    !isLoggingIn &&
-    !isLoggingOut &&
-    !isRegistering &&
-    !isUpdating &&
-    !isDeleting &&
-    form.email && form.email === user?.email &&
-    form.current_password
-  );
+  componentDidMount() {
+    if (this.component.current) disableBodyScroll(this.component.current);
+  }
 
-  const formDeleteUser = (
-    <form
-      className='AccountFormDeleteUser-form'
-      id='formDeleteUser'
-      onSubmit={canSubmit ? handleSubmit : undefined}
-    >
-      <div className='CloseIcon-container'>
-        <CloseIcon
-          type='button'
-          onClick={handleClose}
-          disabled={isDeleting}
-        />
-      </div>
-      <div className='AccountFormDeleteUser-form-text'>
-        To deactivate your account, please re-enter your email and password.
-        You will be removed as a member from all projects.
-        All projects for which you are the admin will be deleted.
-      </div>
-      <Input
-        className='AccountFormDeleteUser-form-input'
-        label='Email address'
-        type='email'
-        name='email__deleteUser'
-        value={form.email}
-        disabled={isDeleting}
-        minLength={1}
-        maxLength={50}
-        onChange={handleInput()}
-        autoFocus
-        required
-      />
-      {error?.email?.map(
-        e => <p key={e.id} className='AccountFormDeleteUser-form-error'>{e.msg}</p>
-      )}
-      <Input
-        className='AccountFormDeleteUser-form-input'
-        label='Password'
-        type='password'
-        name='current_password__deleteUser'
-        value={form.current_password}
-        disabled={isDeleting}
-        maxLength={128}
-        onChange={handleInput()}
-        required
-      />
-      {error?.current_password?.map(
-        e => <p key={e.id} className='AccountFormDeleteUser-form-error'>{e.msg}</p>
-      )}
-      {error?.nonField?.map(
-        e => <p key={e.id} className='AccountFormDeleteUser-form-error'>{e.msg}</p>
-      )}
-      {error?.detail?.map(
-        e => <p key={e.id} className='AccountFormDeleteUser-form-error'>{e.msg}</p>
-      )}
-      <button
-        className='AccountFormDeleteUser-form-button'
-        type='submit'
-        disabled={!canSubmit}
+  componentWillUnmount() {
+    if (this.component.current) enableBodyScroll(this.component.current);
+  }
+
+  render() {
+    const { isDeleting, user } = this.props;
+    const { form, error } = this.state;
+
+    const canSubmit = !!(
+      !isDeleting &&
+      form.email && form.email === user?.email &&
+      form.current_password
+    );
+
+    const formDeleteUser = (
+      <form
+        className='AccountFormDeleteUser-form'
+        id='formDeleteUser'
+        onSubmit={canSubmit ? this.handleSubmit : undefined}
       >
-        {isDeleting ? 'Deleting account...' : 'Confirm delete'}
-      </button>
-    </form>
-  );
-
-  const buttonShowForm = (
-    <button
-      className='AccountButtonDeleteUser'
-      type='button'
-      disabled={formOnDeleteUser || isUpdating || isDeleting}
-      onClick={handleShow}
-    >
-      Deactivate my account
-    </button>
-  );
-
-  return (
-    <div
-      className={`AccountFormDeleteUser${formOnDeleteUser ? ' is-on' : ''}`}
-      ref={thisComponent}
-    >
-      {formOnDeleteUser && (
-        <>
-          <div
-            className='AccountFormDeleteUser-overlay'
-            onClick={handleClose}
+        <div className='CloseIcon-container'>
+          <CloseIcon
+            type='button'
+            onClick={this.handleClose}
+            disabled={isDeleting}
           />
-          {formDeleteUser}
-        </>
-      )}
-      {!formOnDeleteUser && buttonShowForm}
-    </div>
-  );
+        </div>
+        <div className='AccountFormDeleteUser-form-text'>
+          To deactivate your account, please re-enter your email and password.
+          You will be removed as a member from all projects.
+          All projects for which you are the admin will be deleted.
+        </div>
+        <Input
+          className='AccountFormDeleteUser-form-input'
+          label='Email address'
+          type='email'
+          name='email__deleteUser'
+          value={form.email}
+          disabled={isDeleting}
+          minLength={1}
+          maxLength={50}
+          onChange={this.handleInput}
+          autoFocus
+          required
+        />
+        {error?.email?.map(
+          e => <p key={e.id} className='AccountFormDeleteUser-form-error'>{e.msg}</p>
+        )}
+        <Input
+          className='AccountFormDeleteUser-form-input'
+          label='Master password'
+          type='password'
+          name='current_password__deleteUser'
+          value={form.current_password}
+          disabled={isDeleting}
+          onChange={this.handleInput}
+          required
+        />
+        {error?.current_password?.map(
+          e => <p key={e.id} className='AccountFormDeleteUser-form-error'>{e.msg}</p>
+        )}
+        {error?.nonField?.map(
+          e => <p key={e.id} className='AccountFormDeleteUser-form-error'>{e.msg}</p>
+        )}
+        {error?.detail?.map(
+          e => <p key={e.id} className='AccountFormDeleteUser-form-error'>{e.msg}</p>
+        )}
+        <button
+          className='AccountFormDeleteUser-form-button'
+          type='submit'
+          disabled={!canSubmit}
+        >
+          {isDeleting ? 'Deleting account...' : 'Confirm delete'}
+        </button>
+      </form>
+    );
+
+    return (
+      <div className='AccountFormDeleteUser' ref={this.component}>
+        <div
+          className='AccountFormDeleteUser-overlay'
+          onClick={this.handleClose}
+        />
+        {formDeleteUser}
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = (state: AppState) => ({
+  isDeleting: state.user.isDeleting,
+  user: state.user.user,
+});
+
+const mapDispatchToProps = (dispatch: AppDispatch) => ({
+  accountFormDeleteClose: () => {
+    dispatch({ type: 'ACCOUNT_FORM_DELETE_CLOSE' });
+  },
+  startDeleteUser: () => {
+    dispatch({ type: 'START_DELETE_USER' });
+  },
+  stopDeleteUser: () => {
+    dispatch({ type: 'STOP_DELETE_USER' });
+  },
+  successDeleteUser: () => {
+    dispatch({ type: 'SUCCESS_DELETE_USER' });
+  },
+  startLogoutUser: () => {
+    dispatch({ type: 'START_LOGOUT_USER' });
+  },
+});
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+
+type Props = ConnectedProps<typeof connector>;
+
+interface IForm {
+  email: string;
+  current_password: string;
+}
+
+interface State {
+  form: IForm;
+  error: ReturnType<typeof parseErrorResponse>;
+}
+
+export default connector(AccountFormDeleteUser);
